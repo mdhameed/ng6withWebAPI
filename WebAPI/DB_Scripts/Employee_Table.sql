b77dd4d794b3232526aecddfe1b466d8c3e47e36
Mohammed Hameed <hameed.in@hotmail.com>
2018-05-25 15:33:16 +0530

    Added DB_Scripts folder, these scripts have to be run on the db.

---
+++ WebAPI/DB_Scripts/Employee_Table.sql
@@ -0,0 +1,17 @@
+﻿/****** Object: Table [dbo].[Employee] Script Date: 25-05-2018 15:29:21 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Employee] (
+    [EmployeeID] INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName]  VARCHAR (50) NULL,
+    [LastName]   VARCHAR (50) NULL,
+    [EmpCode]    VARCHAR (50) NULL,
+    [Position]   VARCHAR (50) NULL,
+    [Office]     VARCHAR (50) NULL
+);
+
+
